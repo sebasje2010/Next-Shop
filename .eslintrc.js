f4706ba0083bbd2c1ba2b020541540514b7b5c10
sebasje2010 <sebastian.jativa@hotmail.com>
2022-04-28 09:21:02 -0500

    configurado prettier

---
+++ .eslintrc.js
@@ -0,0 +1,18 @@
+module.exports = {
+  env: {
+    browser: true,
+    es6: true,
+    node: true,
+    amd: true,
+  },
+  extends: [
+    "eslint:recommended",
+    "plugin:jsx-a11y/recommended",
+    "plugin:prettier/recommended",
+    "next",
+    "next/core-web-vitals",
+  ],
+  rules: {
+    semi: ["error", "always"],
+  },
+};
